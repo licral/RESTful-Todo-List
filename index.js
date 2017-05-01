cbc26fa8486b3f10a74011912f458fe3f7d9d9c1
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-01 18:36:53 +1200

    Removed a print lin

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.listen(port, function () {
 });
 
 app.get('/get_all_tasks', function (request, response) {
-    console.log(request.query.done);
     var queryString = "select * from todo where done=" + request.query.done + " order by id";
     var query = client.query(queryString);
     var taskHTML = '';

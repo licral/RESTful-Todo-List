d13c6d54cb69a711f09beee035fddb47fcf1f394
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-01 19:21:33 +1200

    edited query string

--- index.js
+++ index.js
@@ -23,12 +23,11 @@ app.listen(port, function () {
 });
 
 app.get('/get_all_tasks', function (request, response) {
-    var queryString = "select * from todo where done=" + request.query.done;
+    var queryString = "select * from todo where done=false";
     var query = client.query(queryString);
     var taskHTML = '';
 
     query.on('row', function (row) {
-        console.log(row);
         taskHTML += '<li id="' + row.id + '"><span class="done">%</span>';
         taskHTML += '<span class="edit">+</span>';
         taskHTML += '<span class="delete">x</span>';

3e0452195cfc4cd7b97d7088c213477ee883a272
liaobonn <liaobonn@ecs.vuw.ac.nz>
2017-05-01 16:29:06 +1200

    Forgot to use version control.... Added all files now

---
+++ index.js
@@ -0,0 +1,150 @@
+/**
+ * Created by liaobonn on 27/03/17.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 3000;
+
+var pg = require('pg');
+var connectionString = 'postgres://liaobonn:300315201@depot:5432/liaobonn_jdbc';
+
+var client = new pg.Client(connectionString);
+client.connect();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, function () {
+    console.log('Listening on port 3000');
+});
+
+app.get('/get_all_tasks', function (request, response) {
+    console.log(request.query.done);
+    var queryString = "select * from todo where done=" + request.query.done + " order by id";
+    var query = client.query(queryString);
+    var taskHTML = '';
+
+    query.on('row', function (row) {
+        taskHTML += '<li id="' + row.id + '"><span class="done">%</span>';
+        taskHTML += '<span class="edit">+</span>';
+        taskHTML += '<span class="delete">x</span>';
+        taskHTML += '<span class="task">' + row.task + '</span></li>';
+    });
+
+    query.on('error', function (err) {
+        console.log(err);
+    });
+
+    query.on('end', function () {
+        console.log("Retrieved all tasks");
+        response.send(taskHTML);
+    })
+});
+
+app.post('/add_task', function (request, response) {
+    var task = request.query.task;
+    if (task != undefined && task != "") {
+
+        var queryString = "select * from todo order by id desc limit 1";
+        var query = client.query(queryString);
+
+        query.on('row', function (row) {
+            var taskHTML = '<li id="' + (row.id + 1) + '"><span class="done">%</span>';
+            taskHTML += '<span class="edit">+</span>';
+            taskHTML += '<span class="delete">x</span>';
+            taskHTML += '<span class="task">' + task + '</span></li>';
+
+            addTask(task);
+
+            response.send(taskHTML);
+        });
+
+        query.on('error', function (err) {
+            console.log(err);
+        });
+
+
+    }
+});
+
+app.post('/complete_task', function (request, response) {
+    var queryString = "update todo set done = true where id = " + request.query.id;
+    var query = client.query(queryString);
+
+    query.on('end', function () {
+        response.sendStatus(200);
+    });
+
+    query.on('error', function (err) {
+        console.log(err);
+    });
+});
+
+app.post('/edit_task', function (request, response) {
+    if (request.query.task != "" && request.query.task != undefined && request.query.id != "") {
+        var queryString = "update todo set task='" + request.query.task + "' where id = " + request.query.id;
+        var query = client.query(queryString);
+
+        query.on('end', function () {
+            console.log("Task has been editted");
+            response.sendStatus(200);
+        });
+
+        query.on('error', function (err) {
+            console.log(err);
+        });
+    }
+});
+
+app.post('/delete_task', function (request, response) {
+    if (request.query.id != "") {
+        var queryString = "delete from todo where id=" + request.query.id;
+        var query = client.query(queryString);
+
+        query.on('end', function () {
+            console.log("Task has been deleted");
+            response.sendStatus(200);
+        });
+
+        query.on('error', function (err) {
+            console.log(err);
+            response.sendStatus(400);
+        });
+    } else {
+        response.sendStatus(400);
+    }
+});
+
+app.post('/update_list', function (request, response) {
+    var allTasks = request.body.allTasks;
+    var queryString = "delete from todo";
+    var query = client.query(queryString);
+
+    query.on('end', function () {
+        allTasks.forEach(function (task) {
+            client.query("insert into todo (task, done) values ('" + task.task + "', " + task.done + ")");
+        })
+    });
+
+    query.on('error', function (err) {
+        console.log(err);
+    });
+});
+
+function addTask(task) {
+    var queryString = "insert into todo (task, done) values ('" + task + "', false)";
+    var query = client.query(queryString);
+
+    query.on('end', function () {
+        console.log("Task has been added");
+    });
+
+    query.on('error', function (err) {
+        console.log(err);
+    });
+}
---
+++ public/js/todo.js
@@ -0,0 +1,194 @@
+$(document).ready(function (e) {
+    
+    loadTodoTasks();
+    loadCompletedTasks();
+    
+    $('#add-todo').button({
+        icons: {
+            primary: "ui-icon-circle-plus"
+        }
+    }).click(function () {
+        $('#new-todo').dialog('open');
+    });
+
+    $('#new-todo').dialog({
+        modal: true,
+        autoOpen: false,
+        buttons: {
+            "Add task": function () {
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://localhost:3000/add_task?task=' + $('#task').val(),
+                    dataType: 'html',
+                    success: function(data){
+                        addTask(data, '#new-todo');
+                        updateList();
+                    }
+                });
+            },
+            "Cancel": function () {
+                $(this).dialog('close');
+            }
+        }
+    });
+
+    $('#todo-list').on('click', '.done', function () {
+        var $taskItem = $(this).parent('li');
+        var task = $taskItem.attr('id');
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/complete_task?id=' + task,
+            success: function(){
+                $taskItem.slideUp(250, function () {
+                    var $this = $(this);
+                    $this.detach();
+                    $('#completed-list').prepend($this);
+                    $this.slideDown();
+                });
+            }
+        });
+
+    });
+
+    $('#todo-list').on('click', '.edit', function(){
+        var taskEdit = $(this).siblings('.task');
+        var taskId = $(this).parent('li').attr('id');
+        $('#task-edit').val(taskEdit.text());
+        $('#edit-todo').data({
+            taskEdit: taskEdit,
+            taskId: taskId
+        }).dialog('open');
+    });
+
+    $('#edit-todo').dialog({
+        modal: true,
+        autoOpen: false,
+        buttons:{
+            "Save": function(){
+                var newItem = $('#task-edit').val();
+                var taskItem = $(this).data('taskEdit');
+                var taskId = $(this).data('taskId');
+                
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://localhost:3000/edit_task?id=' + taskId + '&task=' + newItem,
+                    success: function(){
+                        editTask(taskItem, newItem, '#edit-todo');
+                    }
+                });
+            },
+            "Cancel": function () {
+                $(this).dialog('close');
+            }
+        }
+    });
+
+    $('.sortlist').sortable({
+        connectWith: '.sortlist',
+        cursor: 'pointer',
+        placeholder: 'ui-state-highlight',
+        cancel: '.delete, .done',
+        stop: function(){
+            updateList();
+        }
+    });
+
+    $('.sortlist').on('click', '.delete', function () {
+        var taskDelete = this;
+        var taskId = $(this).parent('li').attr('id');
+        $('#confirm-delete').data({
+            taskItem: taskDelete,
+            taskId: taskId
+        }).dialog('open');
+    });
+
+    $('#confirm-delete').dialog({
+        modal: true,
+        autoOpen: false,
+        buttons: {
+            "Yes": function () {
+                var taskItem = $(this).data('taskItem');
+                var taskId = $(this).data('taskId');
+                $.ajax({
+                    type: 'POST',
+                    url: 'http://localhost:3000/delete_task?id=' + taskId,
+                    success: function(){
+                        deleteTask(taskItem, '#confirm-delete');
+                    }
+                });
+            },
+            "No": function () {
+                $(this).dialog('close');
+            }
+        }
+    });
+
+    function addTask(data, id){
+        $newTask = $(data);
+        $newTask.hide();
+        $('#todo-list').prepend($newTask);
+        $newTask.show('clip', 250).effect('highlight', 1000);
+        $('#task').val("");
+        $(id).dialog('close');
+    }
+
+    function editTask(taskItem, newItem, id){
+        taskItem.text(newItem);
+        $(id).dialog('close');
+    }
+
+    function deleteTask(taskItem, id){
+        $(taskItem).parent('li').effect('puff', function () {
+            $(taskItem).remove();
+        });
+        $(id).dialog('close');
+    }
+    
+    function loadTodoTasks(){
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:3000/get_all_tasks?done=false',
+            dataType: 'html',
+            success: function(data){
+                $('#todo-list').html(data);
+            }
+        });
+    }
+
+    function loadCompletedTasks(){
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:3000/get_all_tasks?done=true',
+            dataType: 'html',
+            success: function(data){
+                $('#completed-list').html(data);
+            }
+        });
+    }
+
+    function updateList(){
+        var allTasks = [];
+        var $todoList = $('.sortlist li');
+        $todoList.each(function(){
+            var task = $(this).children('.task').text();
+            var done = false;
+            if($(this).parent('ul').attr('id') == "completed-list"){
+                done = true;
+            }
+            allTasks.push({
+                task: task,
+                done: done
+            });
+        });
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/update_list',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                allTasks: allTasks
+            })
+        });
+    }
+    
+}); // end ready
